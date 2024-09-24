c2a48a466a079a94869151383ca3a6cb90ec7266
josephruiz18101 <joseph123920@gmail.com>
2024-09-23 22:40:04 -0500

    Add files via upload

---
+++ Module2challenge/assets/css/script.js
@@ -0,0 +1,8 @@
+const openPdfButton = document.getElementById('Resume');
+
+openPdfButton.addEventListener('click', function() {
+    window.open('./assets/files/Joseph-Ruiz.pdf', '_blank');
+});
+//document.getElementById("Email").onclick = function() {
+    //window.location.href = "mailto:joseph123920@gmail.com?subject=Subject%20Here&body=Body%20Here";
+//};
